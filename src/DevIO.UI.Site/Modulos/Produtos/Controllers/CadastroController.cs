512174d686f7551c0135d60d4ea19a00484f9ee2
Ramon Almeida <ramondeallima@gmail.com>
2019-08-24 01:28:00 -0300

    Personalização do nome da pasta 'Areas'

--- src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
+++ src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DevIO.UI.Site.Areas.Produtos.Controllers
+namespace DevIO.UI.Site.Modulos.Produtos.Controllers
 {
     [Area("Produtos")]
     public class CadastroController : Controller
@@ -12,6 +12,7 @@ namespace DevIO.UI.Site.Areas.Produtos.Controllers
         public IActionResult Index()
         {
             return View();
-        }   
+        }
+
     }
 }
