375d64d395b0b6c141bd19905a4e61cf16eca415
Ramon Almeida <ramondeallima@gmail.com>
2019-09-09 22:11:26 -0300

    Trabalhando com Migrations e testando o CRUD

---
+++ src/DevIO.UI.Site/Migrations/20190910005607_RemoveDataCadastro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevIO.UI.Site.Migrations
+{
+    public partial class RemoveDataCadastro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCadastro",
+                table: "Alunos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCadastro",
+                table: "Alunos",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
